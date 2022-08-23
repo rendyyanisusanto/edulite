8a7b8084e6b201382ddf00bb055596ebb04f4447
rendy <rendyyanisusanto@gmail.com>
2022-08-23 09:15:54 +0700

    asd

--- application/views/role/admin/page/alumni/index_page/index.php
+++ application/views/role/admin/page/alumni/index_page/index.php
@@ -21,7 +21,7 @@
         <a class="btn bg-info-400 btn-labeled legitRipple" target="__blank" href="<?php echo $data_get['param']['table'] ?>/print_hari_ini"><b><i class="icon-printer2"></i></b> Print hari ini</a>
         <hr>
         <div class="table-responsive">
-        <table class="table" width="100%" id="tabel-data">
+        <table class="table table-xxs table-framed" width="100%" id="tabel-data">
           <thead>
             <tr>
               <th width="1%">#</th>
