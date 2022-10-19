e3493bdeab008f5a92e489ff6e751aeb3ad161ac
rendy <rendyyanisusanto@gmail.com>
2022-10-19 17:01:41 +0700

    update tgl 19-10-2022

--- application/views/role/admin/page/siswa/add_page/index.php
+++ application/views/role/admin/page/siswa/add_page/index.php
@@ -1,5 +1,5 @@
 <script type="text/javascript" src="<?php echo base_url('include/ckeditor/ckeditor.js'); ?>"></script>
-<script src="<?php echo base_url('include/template/limitless/')?>/global_assets/js/plugins/forms/selects/select2.min.js"></script>
+<script src="<?php echo base_url('include/template/dashboard/')?>js/plugins/forms/selects/select2.min.js"></script>
 <form class="form-horizontal" action="<?php echo $data_get['param']['table'] ?>/simpan_data" id="app-submit" method="POST">
   <div class="row">
     <div class="col-md-8">
