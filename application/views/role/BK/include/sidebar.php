e282516827de157e185ce8443d12671f068164f5
Your Name <you@example.com>
2024-12-11 13:21:36 +0700

    asd

--- application/views/role/BK/include/sidebar.php
+++ application/views/role/BK/include/sidebar.php
@@ -1,33 +1,10 @@
 <ul class="navigation navigation-main navigation-accordion">
   <!-- Main -->
   <li class="navigation-header"><span>Main</span> <i class="icon-menu" title="Main pages"></i></li>
-  <li><a href="Dashboard/get_data" class="app-item"><i class="icon-home4"></i> <span>Dashboard</span></a></li>
+  <li><a href="Dashboard/get_data" class="app-item"><i class="icon-home4"></i> <span>MENU</span></a></li>
   
  
-  <!-- Page kits -->
-  <li class="navigation-header"><span>Role Management</span> <i class="icon-menu" title="Page kits"></i></li>
-  <!-- <li>
-    <a href="#"><i class="icon-book"></i> <span>Catatan Siswa</span></a>
-    <ul>
-      <li><a href="Pelanggaran_siswa/get_data" class="app-item">Kartu Kendali Siswa</a></li>
-      <li><a href="Buku_pemanggilan_siswa/get_data" class="app-item">Buku Pemanggilan Siswa</a></li>
-      <li><a href="Pelanggaran_siswa/catatan_siswa" class="app-item">Catatan Siswa Oleh Guru</a></li>
-    </ul>
-  </li>
-  <li>
-    <a href="#"><i class="icon-users2"></i> <span>Buku Alumni</span></a>
-    <ul>
-      <li><a href="Alumni/get_data" class="app-item">Data Alumni</a></li>
-      <li><a href="Alumni/persebaran_alumni" class="app-item">Persebaran Alumni</a></li>
-    </ul>
-  </li> -->
-  <li><a href="Presensi_harian/presensi_siswa_masuk_pulang" class="app-item"><i class=" icon-paste4"></i> <span>Presensi Siswa (Masuk-Pulang)</span></a></li>
-  <li><a href="Buku_pemanggilan_siswa/get_data" class="app-item"><i class=" icon-paste4"></i> <span>Buku Pemanggilan Siswa</span></a></li>
-  <li><a href="Alumni/get_data" class="app-item"><i class=" icon-paste4"></i> <span>Data Alumni</span></a></li>
-  <!-- <li><a href="Pelanggaran_siswa/poin_pelanggaran_setup" class="app-item"><i class=" icon-paste4"></i> <span>Point Pelanggaran</span></a></li> -->
-  <li><a href="Presensi_harian/rekap" class="app-item"><i class=" icon-paste4"></i> <span>Rekap Presensi Siswa</span></a></li>
-  <li class="navigation-header"><span>Setting kits</span> <i class="icon-menu" title="Page kits"></i></li>
-  
+ 
   <li>
     <a href="#"><i class="icon-users2"></i> <span>Account</span></a>
     
